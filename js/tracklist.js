7cc8e467dd942927090197713c38d3eaea7fae00
Chris Lowles <lowleschris@gmail.com>
2026-01-21 19:51:33 +1100

    Update tracklist.js

--- js/tracklist.js
+++ js/tracklist.js
@@ -8,6 +8,7 @@ jQuery(document).ready(function($) {
 		axis: 'y',
 		update: function() {
 			calculateTotalDuration();
+			updateYouTubePlaylistLink();
 		}
 	});
 
@@ -57,68 +58,96 @@ jQuery(document).ready(function($) {
 		return (match && match[7].length == 11) ? match[7] : null;
 	}
 
-	// 6. Function to fetch duration from YouTube
-	function fetchYouTubeDuration(button) {
+	// 6. NEW: Function to check if URL is a YouTube URL
+	function isYouTubeURL(url) {
+		if (!url) return false;
+		return url.includes('youtube.com') || url.includes('youtu.be');
+	}
+
+	// 7. NEW: Function to update YouTube playlist link
+	function updateYouTubePlaylistLink() {
+		var videoIds = [];
+		var allYouTube = true;
+		var hasAnyTracks = false;
+
+		// Collect all video IDs from track rows (not spacers)
+		container.find('.track-row:not(.is-spacer)').each(function() {
+			var urlInput = $(this).find('.track-url-input');
+			var url = urlInput.val();
+			
+			if (url) {
+				hasAnyTracks = true;
+				if (isYouTubeURL(url)) {
+					var videoId = extractYouTubeID(url);
+					if (videoId) {
+						videoIds.push(videoId);
+					}
+				} else {
+					allYouTube = false;
+				}
+			}
+		});
+
+		// Show or hide the YouTube playlist link
+		if (allYouTube && videoIds.length > 0 && hasAnyTracks) {
+			var playlistUrl = 'https://www.youtube.com/watch_videos?video_ids=' + videoIds.join(',');
+			
+			// Create or update the link
+			if ($('#youtube-playlist-link').length === 0) {
+				$('#total-duration').parent().after(
+					'<div style="font-size: 13px; margin-top: 8px;">' +
+					'<a id="youtube-playlist-link" href="' + playlistUrl + '" target="_blank" rel="noopener noreferrer" class="button button-secondary" style="text-decoration: none;">' +
+					'Open All in YouTube' +
+					'</a>' +
+					'</div>'
+				);
+			} else {
+				$('#youtube-playlist-link').attr('href', playlistUrl);
+			}
+		} else {
+			$('#youtube-playlist-link').parent().remove();
+		}
+	}
+
+	// 8. Function to fetch duration from any supported platform
+	function fetchDuration(button) {
 		var row = button.closest('.track-row');
 		var urlInput = row.find('.track-url-input');
 		var durationInput = row.find('.track-duration-input');
 		var url = urlInput.val();
 		
 		if (!url) {
-			alert('Please enter a valid URL first (YouTube).');
-			return;
-		}
-		
-		var videoId = extractYouTubeID(url);
-		if (!videoId) {
-			alert('URL not valid for grabbing duration. Please check the URL and try again or grab duration manually.');
+			alert('Please enter a valid URL first.');
 			return;
 		}
 		
 		// Disable button and show loading state
 		button.prop('disabled', true).text('Grabbing Duration');
 		
-		// Use YouTube oEmbed API (no API key required)
+		// Use noembed.com which supports multiple platforms
+		// (YouTube, Vimeo, SoundCloud, Dailymotion, etc.)
 		$.ajax({
-			url: 'https://www.youtube.com/oembed',
-			data: {
-				url: 'https://www.youtube.com/watch?v=' + videoId,
-				format: 'json'
-			},
+			url: 'https://noembed.com/embed',
+			data: { url: url },
 			dataType: 'json',
 			success: function(data) {
-				// oEmbed doesn't provide duration, so we need to scrape it differently
-				// Let's use the noembed.com service which provides more data
-				$.ajax({
-					url: 'https://noembed.com/embed',
-					data: {
-						url: 'https://www.youtube.com/watch?v=' + videoId
-					},
-					dataType: 'json',
-					success: function(embedData) {
-						if (embedData.duration) {
-							durationInput.val(formatDuration(embedData.duration));
-							calculateTotalDuration();
-							button.prop('disabled', false).text('Grab Duration');
-						} else {
-							alert('Sorry, couldn\'t grab duration. Please get the duration manually.');
-							button.prop('disabled', false).text('Grab Duration');
-						}
-					},
-					error: function() {
-						alert('Sorry, couldn\'t grab duration. Please get the duration manually.');
-						button.prop('disabled', false).text('Grab Duration');
-					}
-				});
+				if (data.duration) {
+					durationInput.val(formatDuration(data.duration));
+					calculateTotalDuration();
+					button.prop('disabled', false).text('Grab Duration');
+				} else {
+					alert('Duration not available for this URL. Please enter it manually.');
+					button.prop('disabled', false).text('Grab Duration');
+				}
 			},
 			error: function() {
-				alert('Sorry, couldn\'t grab duration. Please get the duration manually.');
+				alert('Couldn\'t fetch duration. Please enter it manually.');
 				button.prop('disabled', false).text('Grab Duration');
 			}
 		});
 	}
 
-	// 7. Function to add a new row
+	// 9. Function to add a new row
 	function addRow(type) {
 		var index = container.find('.track-row').length;
 		
@@ -142,28 +171,36 @@ jQuery(document).ready(function($) {
 		`;
 		container.append(html);
 		calculateTotalDuration();
+		updateYouTubePlaylistLink();
 	}
 
-	// 8. Button Click Events
+	// 10. Button Click Events
 	$('.add-track').on('click', function() { addRow('track'); });
 	$('.add-spacer').on('click', function() { addRow('spacer'); });
 
-	// 9. Remove Button Event (Delegated for dynamically added items)
+	// 11. Remove Button Event (Delegated for dynamically added items)
 	container.on('click', '.remove-track', function() {
 		$(this).closest('.track-row').remove();
 		calculateTotalDuration();
+		updateYouTubePlaylistLink();
 	});
 
-	// 10. Fetch Duration Button Event (Delegated for dynamically added items)
+	// 12. Fetch Duration Button Event (Delegated for dynamically added items)
 	container.on('click', '.fetch-duration', function() {
-		fetchYouTubeDuration($(this));
+		fetchDuration($(this));
 	});
 
-	// 11. Update total when duration changes
+	// 13. Update total when duration changes
 	container.on('input', '.track-duration-input', function() {
 		calculateTotalDuration();
 	});
 
-	// 12. Calculate initial total on page load
+	// 14. NEW: Update YouTube link when URL changes
+	container.on('input', '.track-url-input', function() {
+		updateYouTubePlaylistLink();
+	});
+
+	// 15. Calculate initial total and YouTube link on page load
 	calculateTotalDuration();
+	updateYouTubePlaylistLink();
 });
