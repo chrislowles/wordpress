b6d7fb6f2043896b0b1de5435c01505cb91dfd86
Chris Lowles <lowleschris@gmail.com>
2026-01-25 19:24:13 +1100

    Update entry-content.php

--- entry-content.php
+++ entry-content.php
@@ -7,35 +7,6 @@
 			</a>
 		<?php endif; ?>
 	</div>
-	<?php
-		// 1. Retrieve the tracklist data from the current post
-		$tracklist = get_post_meta(get_the_ID(), 'tracklist', true);
-		// 2. Check if we actually have data before printing anything
-		if (!empty($tracklist) && is_array($tracklist)):
-	?>
-		<h3>Tracklist/Timeline:</h3>
-		<ul class="tracklist-display">
-			<?php foreach ($tracklist as $item):
-				// Get the type (track or spacer) to use as a CSS class
-				$type = isset($item['type']) ? esc_attr($item['type']) : 'track';
-				$title = isset($item['track_title']) ? esc_html($item['track_title']) : '';
-				$url = isset($item['track_url']) ? esc_url($item['track_url']) : '';
-			?>
-				<li class="track-item type-<?php echo $type; ?>">
-					<?php
-					// Logic: If it's a 'track' AND has a URL, make it a link.
-					// Otherwise (spacers or tracks without links), just show text.
-					if ($type === 'track' && !empty($url)) : ?>
-						<a href="<?php echo $url; ?>" target="_blank" rel="noopener noreferrer">
-							<?php echo $title; ?>
-						</a>
-					<?php else: ?>
-						<span class="track-title"><?php echo $title; ?></span>
-					<?php endif; ?>
-				</li>
-			<?php endforeach; ?>
-		</ul>
-	<?php endif; ?>
 	<div class="body-content">
 		<?php the_content(); ?>
 	</div>
