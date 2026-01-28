be1750268cd40445e04d43928dbe4aafa4521d27
Chris Lowles <lowleschris@gmail.com>
2026-01-29 03:10:04 +1100

    Update disable-comments.php

--- inc/disable-comments.php
+++ inc/disable-comments.php
@@ -2,6 +2,7 @@
 
 // COMPLETELY DISABLE COMMENTS/TRACKBACKS/PINGBACKS.
 // Removes UI elements from Admin Menu, Admin Bar, and Front End.
+
 // 1. Disable support for comments and trackbacks in post types
 add_action('admin_init', function() {
 	$post_types = get_post_types();
@@ -12,15 +13,19 @@ add_action('admin_init', function() {
 		}
 	}
 });
+
 // 2. Close comments on the front-end
 add_filter('comments_open', '__return_false', 20, 2);
 add_filter('pings_open', '__return_false', 20, 2);
+
 // 3. Hide existing comments
 add_filter('comments_array', '__return_empty_array', 10, 2);
+
 // 4. Remove comments page in menu
 add_action('admin_menu', function() {
 	remove_menu_page('edit-comments.php');
 });
+
 // 5. Redirect any user trying to access comments page
 add_action('admin_init', function () {
 	global $pagenow;
@@ -29,11 +34,13 @@ add_action('admin_init', function () {
 		exit;
 	}
 });
+
 // 6. Remove comments from the Admin Bar
 add_action('wp_before_admin_bar_render', function () {
 	global $wp_admin_bar;
 	$wp_admin_bar->remove_menu('comments');
 });
+
 // 7. Remove "Recent Comments" style from head
 add_action('widgets_init', function () {
 	global $wp_widget_factory;
@@ -41,15 +48,18 @@ add_action('widgets_init', function () {
 		remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
 	}
 });
+
 // 8. "Recent Comments" widget
 add_action('widgets_init', function() {
 	unregister_widget('WP_Widget_Recent_Comments');
 });
+
 // 9. Remove X-Pingback to header
 add_filter('wp_headers', function ($headers) {
 	unset($headers['X-Pingback']);
 	return $headers;
 });
+
 // 10. Hide the comment form on front end (CSS Fallback)
 // This is a safety net for themes that hardcode the comment form without checking if comments are open.
 add_action('wp_head', function () {
