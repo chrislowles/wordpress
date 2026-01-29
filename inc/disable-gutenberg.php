d06ec5c53ac49fb4db43ee47a816b99d4e9cdea8
Chris Lowles <lowleschris@gmail.com>
2026-01-29 22:44:21 +1100

    Disable Gutenberg and visual editor globally
    Disables the Gutenberg block editor and the TinyMCE visual editor for all users, enforcing the classic code editor. Also ensures the default editor is set to HTML and cleans up related frontend assets.

--- inc/disable-gutenberg.php
+++ inc/disable-gutenberg.php
@@ -2,11 +2,23 @@
 
 // Disable Gutenberg completely - Markup Markdown works with Classic Editor
 
+// 1. DISABLE GUTENBERG (BLOCK EDITOR)
 // BACKEND
 add_filter('use_block_editor_for_post', '__return_false');
 
 // WIDGETS
 add_filter('use_widgets_block_editor', '__return_false');
+
+// 2. DISABLE VISUAL EDITOR (TINYMCE)
+// This removes the "Visual" tab and enforces "Text" (Code) mode globally.
+add_filter('user_can_richedit', '__return_false');
+
+// Force the default editor to always be 'html' (just to be safe)
+add_filter('wp_default_editor', function() {
+	return 'html';
+});
+
+// 3. CLEAN UP FRONTEND ASSETS
 add_action('wp_enqueue_scripts', function() {
 	// Remove CSS on the front end.
 	wp_dequeue_style('wp-block-library');
