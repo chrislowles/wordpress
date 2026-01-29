356a9c1017defc9c466bca508ab30d137f45325a
Chris Lowles <lowleschris@gmail.com>
2026-01-30 06:08:27 +1100

    Remove commented-out multi-authors list code
    Deleted an unused commented line for displaying multiple authors in entry-meta.php to clean up the code.

--- entry-meta.php
+++ entry-meta.php
@@ -1,7 +1,6 @@
 <!-- entry-meta.php -->
 <div class="entry meta">
 	<?php the_author_posts_link(); ?>
-	<?php // echo get_multi_authors_list(); ?>
 	<span class="sep">|</span>
 	<span class="hyperlink">
 		<a href="<?php the_permalink(); ?>" class="date" title="<?php echo esc_attr(get_the_date()); ?>">
