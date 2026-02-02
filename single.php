08ff25b6673edccfc389c4097a54963b6818caf4
Chris Lowles <lowleschris@gmail.com>
2026-02-03 01:44:52 +1100

    Update single.php

--- single.php
+++ single.php
@@ -2,13 +2,13 @@
 	<?php if (have_posts()): while (have_posts()): the_post(); ?>
 		<?php
 			if ( get_post_type() === 'show' ) {
-				get_template_part('parts/content', 'show');
+				get_template_part( 'parts/content', 'show' );
 			} else {
-				get_template_part('parts/content', 'single');
+				get_template_part( 'parts/content', 'single' );
 			}
 		?>
 	<?php endwhile; endif; ?>
 	<footer>
-		<?php get_template_part('nav', 'below-single'); ?>
+		<?php get_template_part( 'nav', 'below-single' ); ?>
 	</footer>
 <?php get_footer(); ?>
