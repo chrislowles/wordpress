1d6baef2c4cb7b10c4d858163bbd26e9cada6cf6
Chris Lowles <lowleschris@gmail.com>
2026-02-11 20:32:28 +1100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!-- footer.php -->
 			</main>
 			<?php // get_sidebar(); ?>
-			<footer>&copy; <?php echo esc_html( date_i18n( __( 'Y', 'child' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?></footer>
+			<footer id="footer">&copy; <?php echo esc_html( date_i18n( __( 'Y', 'child' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?></footer>
 		<?php wp_footer(); ?>
 	</body>
 </html>
