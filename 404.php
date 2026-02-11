7f16514137a6acf4ddcf960b1b4a56375aee6ee6
Chris Lowles <lowleschris@gmail.com>
2026-02-12 00:14:03 +1100

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,12 +1,21 @@
 <!-- 404.php -->
 <?php get_header(); ?>
+
 	<article id="post-0" class="post not-found">
-		<header class="header">
-			<h1 class="entry-title">Not Found</h1>
+
+		<header>
+
+			<h1>Not Found</h1>
+
 		</header>
-		<div class="entry-content">
+
+		<div class="content">
+
 			<p>Nothing found for the requested page. Try a search instead?</p>
+
 		</div>
+
 	</article>
+
 <?php get_footer(); ?>
 <!-- /404.php -->
