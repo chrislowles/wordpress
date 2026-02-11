e7b3fc1103072969525a3446193025d1e077c4df
Chris Lowles <lowleschris@gmail.com>
2026-02-12 00:14:01 +1100

    Update entry-meta.php

--- parts/entry-meta.php
+++ parts/entry-meta.php
@@ -1,14 +1,17 @@
-<?php
-/**
- * Template part for displaying post meta (date, author).
- */
-?>
 <div class="entry meta">
+
 	<?php the_author_posts_link(); ?>
+
 	<span class="sep">|</span>
+
 	<span class="hyperlink">
+
 		<a href="<?php the_permalink(); ?>" class="date" title="<?php echo esc_attr( get_the_date() ); ?>">
+
 			<?php the_time( get_option( 'date_format' ) ); ?>
+
 		</a>
+
 	</span>
+
 </div>
