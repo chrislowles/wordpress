bd9d32ca4e7081819bd0a8b69e12a7c9420eb85a
Chris Lowles <lowleschris@gmail.com>
2026-02-12 00:11:49 +1100

    whitespace tweaks

--- parts/content-show.php
+++ parts/content-show.php
@@ -1,23 +1,33 @@
-<?php
-/**
- * Template part for displaying Show CPT posts.
- */
-?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
 	<header>
+
 		<h2>
+
 			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+
 		</h2>
+
 		<?php // get_template_part( 'parts/entry', 'meta-show' ); ?>
+
 	</header>
+
 	<div class="entry">
+
 		<div class="thumbnail">
-			<?php if (has_post_thumbnail()): ?>
-				<a class="thumbnail-link" href="<?php the_post_thumbnail_url( 'full' ); ?>" title="<?php $attachment_id = get_post_thumbnail_id( $post->ID ); the_title_attribute( array( 'post' => get_post($attachment_id ) ) ); ?>">
-					<?php the_post_thumbnail( 'full'/*, array('itemprop' => 'image')*/); ?>
+
+			<?php if ( has_post_thumbnail() ): ?>
+
+				<a class="thumbnail-link" href="<?php the_post_thumbnail_url( 'full' ); ?>">
+
+					<?php the_post_thumbnail( 'full'/*, array('itemprop' => 'image')*/ ); ?>
+
 				</a>
+
 			<?php endif; ?>
+
 		</div>
+
 		<?php $tracklist = get_post_meta($post->ID, 'tracklist', true); if (is_array($tracklist) && !empty($tracklist)): ?>
 			<div class="tracklist-display">
 				<h3>Tracklist / Timeline</h3>
@@ -31,45 +41,78 @@
 						$link_to_section = $track['link_to_section'] ?? false;
 					?>
 						<?php if ($type === 'spacer'): ?>
+
 							<li class="track-item type-spacer">
+
 								<?php if ($link_to_section): ?>
+
 									<a href="#<?php echo esc_attr( sanitize_title( $title ) ); ?>">
 										<?php echo esc_html( $title ); ?>
 									</a>
+
 								<?php else: ?>
+
 									<?php echo esc_html( $title ); ?>
+
 								<?php endif; ?>
+
 							</li>
+
 						<?php else: ?>
+
 							<li class="track-item type-track">
+
 								<span class="track-title">
+
 									<?php if ( !empty( $url ) ): ?>
+
 										<a href="<?php echo esc_url( $url ); ?>" target="_blank" rel="noopener noreferrer">
 											<?php echo esc_html( $title ); ?>
 										</a>
+
 									<?php else: ?>
+
 										<?php echo esc_html( $title ); ?>
+
 									<?php endif; ?>
+
 								</span>
+
 								<?php if ( !empty( $duration ) ): ?>
+
 									<span class="track-duration">
 										[<?php echo esc_html( $duration ); ?>]
 									</span>
+
 								<?php endif; ?>
+
 							</li>
+
 						<?php endif; ?>
+
 					<?php endforeach; ?>
+
 				</ul>
+
 			</div>
+
 		<?php endif; ?>
+
 		<div class="content">
+
 			<?php
 				// the_content();
 				echo nl2div(apply_filters('the_content', get_the_content()));
 			?>
+
 		</div>
+
 		<div class="links">
+
 			<?php wp_link_pages(); ?>
+
 		</div>
+
 	</div>
+
 </article>
--- parts/content-summary.php
+++ parts/content-summary.php
@@ -1,29 +1,41 @@
-<?php
-/**
- * Template part for displaying post summaries (archives, search, index).
- */
-?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header>
-		<h2 class="entry-title">
+
+		<h2>
+
 			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+
 		</h2>
+
 		<?php if (!is_search()): get_template_part('parts/entry-meta'); endif; ?>
+
 	</header>
-	
-	<div class="entry summary">
-		<?php if (has_post_thumbnail() && !is_search()): ?>
-			<a class="thumbnail-link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-				<?php the_post_thumbnail(); ?>
-			</a>
-		<?php endif; ?>
+
+	<div class="entry">
+
+		<div class="thumbnail">
+
+			<?php if ( has_post_thumbnail() && !is_search() ): ?>
+
+				<a class="thumbnail-link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+
+					<?php the_post_thumbnail(); ?>
+
+				</a>
+
+			<?php endif; ?>
+
+		</div>
 		
-		<div><?php the_excerpt(); ?></div>
+		<div class="excerpt">
+			<?php the_excerpt(); ?>
+		</div>
 		
-		<?php if (is_search()): ?>
+		<?php if ( is_search() ): ?>
 			<div class="links"><?php wp_link_pages(); ?></div>
 		<?php endif; ?>
+
 	</div>
 
 </article>
