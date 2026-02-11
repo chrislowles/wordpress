73028352d471ac9824e7ab5055eb63fa12fcbc58
Chris Lowles <lowleschris@gmail.com>
2026-02-11 20:39:34 +1100

    Update entry-meta.php

--- parts/entry-meta.php
+++ parts/entry-meta.php
@@ -7,8 +7,8 @@
 	<?php the_author_posts_link(); ?>
 	<span class="sep">|</span>
 	<span class="hyperlink">
-		<a href="<?php the_permalink(); ?>" class="date" title="<?php echo esc_attr(get_the_date()); ?>">
-			<?php the_time(get_option('date_format')); ?>
+		<a href="<?php the_permalink(); ?>" class="date" title="<?php echo esc_attr( get_the_date() ); ?>">
+			<?php the_time( get_option( 'date_format' ) ); ?>
 		</a>
 	</span>
 </div>
