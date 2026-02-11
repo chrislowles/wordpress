84a73344a3fd18308f0b73921558f3587e939ce5
Chris Lowles <lowleschris@gmail.com>
2026-02-11 20:39:36 +1100

    Update nav-below.php

--- parts/nav-below.php
+++ parts/nav-below.php
@@ -1,8 +1,8 @@
 <!-- nav-below.php -->
 <?php
 	the_posts_navigation(array(
-		'prev_text' => sprintf(esc_html__( '%s older', 'blankslate'), '<span class="meta-nav">&larr;</span>'),
-		'next_text' => sprintf(esc_html__('newer %s', 'blankslate'), '<span class="meta-nav">&rarr;</span>')
+		'prev_text' => sprintf(esc_html__( '%s older', 'child'), '<span class="meta-nav">&larr;</span>'),
+		'next_text' => sprintf(esc_html__('newer %s', 'child'), '<span class="meta-nav">&rarr;</span>')
 	));
 ?>
 <!-- /nav-below.php -->
