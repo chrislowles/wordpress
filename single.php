9f601bd28044f63bb653502c3028e9c92839b1fc
Chris Lowles <lowleschris@gmail.com>
2026-03-02 22:51:52 +1100

    Update single.php

--- single.php
+++ single.php
@@ -1,3 +1,4 @@
+<!-- single.php -->
 <?php get_header(); ?>
 
 	<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
@@ -25,3 +26,4 @@
 	</footer>
 
 <?php get_footer(); ?>
+<!-- /single.php -->
