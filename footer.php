7153ac514ebd607ee25576da26756a03c7521e0a
Chris Lowles <lowleschris@gmail.com>
2026-03-12 02:57:32 +1100

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,6 +3,16 @@
 	    <footer id="footer" class="border-top py-3 mt-4">
         	<div class="container text-muted small">
     	        &copy; <?php echo esc_html( date_i18n('Y') ); ?> <?php echo esc_html( get_bloginfo('name') ); ?>
+				<?php
+					wp_nav_menu(
+						array(
+							// 'menu_class'     => 'navbar-nav justify-content-end flex-grow-1 pe-3',
+							'container'      => false, // Removes the extra <div> wrapper
+							'fallback_cb'    => false, // Prevents falling back to a page list
+							'theme_location' => 'footer',
+						)
+					);
+				?>
 	        </div>
 	    </footer>
 	    <?php wp_footer(); ?>
