0179d2c0b1a3f69c5c9b200bf21746c829efbe87
Chris Lowles <lowleschris@gmail.com>
2026-03-12 02:33:30 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!-- index.php -->
 <?php get_header(); ?>
 	<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
-		<?php get_template_part( 'parts/content', 'summary' ); ?>
+		<?php get_template_part( 'parts/content', 'single' ); ?>
 	<?php endwhile; endif; ?>
 	<?php get_template_part( 'parts/nav', 'below' ); ?>
 <?php get_footer(); ?>
