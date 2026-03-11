03670d3b38b9d805efb2f515d6269836279eef58
Chris Lowles <lowleschris@gmail.com>
2026-03-11 23:34:49 +1100

    Update header.php

--- header.php
+++ header.php
@@ -24,35 +24,18 @@
                         <?php
                             wp_nav_menu(
                                 array(
-                                    //'menu'              => "", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
-                                    'menu_class'        => 'navbar-nav justify-content-end flex-grow-1 pe-3', // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
-                                    //'menu_id'           => "", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
-                                    'container'         => false, // (string) Whether to wrap the ul, and what to wrap it with. Default 'div'.
-                                    //'container_class'   => "", // (string) Class that is applied to the container. Default 'menu-{menu slug}-container'.
-                                    //'container_id'      => "", // (string) The ID that is applied to the container.
-                                    //'fallback_cb'       => "", // (callable|bool) If the menu doesn't exists, a callback function will fire. Default is 'wp_page_menu'. Set to false for no fallback.
-                                    //'before'            => "", // (string) Text before the link markup.
-                                    //'after'             => "", // (string) Text after the link markup.
-                                    'link_before'       => '<span>', // (string) Text before the link text.
-                                    'link_after'        => '</span>', // (string) Text after the link text.
-                                    //'echo'              => "", // (bool) Whether to echo the menu or return it. Default true.
-                                    //'depth'             => "", // (int) How many levels of the hierarchy are to be included. 0 means all. Default 0.
-                                    //'walker'            => "", // (object) Instance of a custom walker class.
-                                    'theme_location'    => is_user_logged_in() ? 'logged-in-menu' : 'logged-out-menu', // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
-                                    //'items_wrap'        => "", // (string) How the list items should be wrapped. Default is a ul with an id and class. Uses printf() format with numbered placeholders.
-                                    //'item_spacing'      => "", // (string) Whether to preserve whitespace within the menu's HTML. Accepts 'preserve' or 'discard'. Default 'preserve'.
+                                    'menu_class'        => 'navbar-nav justify-content-end flex-grow-1 pe-3',
+                                    // 'container'         => false,
+                                    'link_before'       => '<span>',
+                                    'link_after'        => '</span>',
+                                    'theme_location'    => is_user_logged_in() ? 'logged-in-menu' : 'logged-out-menu',
+                                    // 'theme_location'    => 'main-menu',
                                 )
                             );
                         ?>
-                        <li class="nav-item">
-                            <a class="nav-link">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
                         <form class="d-flex mt-3 mt-lg-0" action="/" method="get" role="search">
                             <input type="text" name="s" id="search" aria-label="Search" placeholder="Search" value="<?php the_search_query(); ?>" class="form-control me-2" />
-                            <button class="btn btn-outline" type="submit">Search</button>
+                            <button class="btn btn-outline-success" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
