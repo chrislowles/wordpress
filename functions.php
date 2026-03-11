cc4793a92ba27fd0d2de559d451df2bd66fcd90e
Chris Lowles <lowleschris@gmail.com>
2026-03-11 17:47:18 +1100

    Update functions.php

--- functions.php
+++ functions.php
@@ -34,8 +34,8 @@ add_action('admin_head', function () {
 });
 
 add_action('wp_enqueue_scripts', function() {
-    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), null);
-    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array(), null, true);
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css', array(), null);
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js', array(), null, true);
 });
 
 add_action('admin_enqueue_scripts', function () {
