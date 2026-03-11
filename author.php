bf4f872cf428c307242f879b39b5ac6616e7d2d3
Chris Lowles <lowleschris@gmail.com>
2026-03-11 23:46:06 +1100

    Update author.php

--- author.php
+++ author.php
@@ -1,11 +1,8 @@
+<!-- author.php -->
 <?php get_header(); ?>
-
 	<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
-
 		<?php get_template_part( 'parts/content', 'summary' ); ?>
-
 	<?php endwhile; endif; ?>
-
 	<?php get_template_part( 'parts/nav', 'below' ); ?>
-
 <?php get_footer(); ?>
+<!-- /author.php -->
