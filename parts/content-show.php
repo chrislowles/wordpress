2eaa1ca73770fc0ad902c112889047447906740f
Chris Lowles <lowleschris@gmail.com>
2026-03-12 05:53:00 +1100

    Update content-show.php

--- parts/content-show.php
+++ parts/content-show.php
@@ -5,44 +5,44 @@
 			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
 		</h2>
 	</header>
-	<div class="entry">
-		<div class="thumbnail">
+	<div>
+		<div>
 			<?php if ( has_post_thumbnail() ): ?>
-				<a class="thumbnail-link" href="<?php the_post_thumbnail_url( 'full' ); ?>">
+				<a href="<?php the_post_thumbnail_url( 'full' ); ?>">
 					<?php the_post_thumbnail( 'full' ); ?>
 				</a>
 			<?php endif; ?>
 		</div>
-		<?php $tracklist = get_post_meta( $post->ID, 'tracklist', true ); if ( is_array( $tracklist ) && ! empty( $tracklist ) ): ?>
-			<div class="tracklist-display">
+		<?php $tracklist = get_post_meta($post->ID, 'tracklist', true); if (is_array($tracklist) && !empty($tracklist)): ?>
+			<div class="mt-4">
 				<h3>Tracklist / Timeline</h3>
-				<ul class="list-group">
-					<?php foreach ( $tracklist as $track ):
+				<ul class="list-group mb-4">
+					<?php foreach ($tracklist as $track):
 						$type = $track['type'] ?? 'track';
 						$title = $track['title'] ?? $track['track_title'] ?? '';
 						$dur = $track['duration'] ?? '';
 						$url = $track['url'] ?? $track['track_url'] ?? '';
 						$link = $track['link_to_section'] ?? false;
 					?>
-						<?php if ( $type === 'spacer' ): ?>
-							<li class="list-group-item track-item type-spacer">
-								<?php if ( $link ): ?>
-									<a href="#<?php echo esc_attr( sanitize_title( $title ) ); ?>"><?php echo esc_html( $title ); ?></a>
+						<?php if ($type === 'spacer'): ?>
+							<li class="list-group-item">
+								<?php if ($link): ?>
+									<a href="#<?php echo esc_attr(sanitize_title($title)); ?>"><?php echo esc_html($title); ?></a>
 								<?php else: ?>
-									<?php echo esc_html( $title ); ?>
+									<?php echo esc_html($title); ?>
 								<?php endif; ?>
 							</li>
 						<?php else: ?>
-							<li class="list-group-item track-item type-track">
-								<span class="track-title">
-									<?php if ( ! empty( $url ) ): ?>
-										<a href="<?php echo esc_url( $url ); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html( $title ); ?></a>
+							<li class="list-group-item">
+								<span>
+									<?php if (!empty($url)): ?>
+										<a href="<?php echo esc_url($url); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($title); ?></a>
 									<?php else: ?>
-										<?php echo esc_html( $title ); ?>
+										<?php echo esc_html($title); ?>
 									<?php endif; ?>
 								</span>
-								<?php if ( ! empty( $dur ) ): ?>
-									<span class="track-duration">[<?php echo esc_html( $dur ); ?>]</span>
+								<?php if (!empty($dur)): ?>
+									<small class="text-white-50 fw-normal"><?php echo esc_html($dur); ?></small>
 								<?php endif; ?>
 							</li>
 						<?php endif; ?>
@@ -50,10 +50,10 @@
 				</ul>
 			</div>
 		<?php endif; ?>
-		<div class="content">
+		<div>
 			<?php the_content(); ?>
 		</div>
-		<div class="links">
+		<div>
 			<?php wp_link_pages(); ?>
 		</div>
 	</div>
