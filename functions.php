3136428ef3335c57c01e68777ca40ae04da2c9a5
Chris Lowles <lowleschris@gmail.com>
2026-03-11 22:59:07 +1100

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,18 +22,21 @@ new ChrisLowles_Scratchpad();
 
 // 5. General Theme Setup
 add_action('after_setup_theme', function () {
-	add_theme_support('title-tag');
-	add_theme_support('post-thumbnails');
-	add_theme_support('html5', array('search-form', 'gallery', 'caption'));
-	register_nav_menus( array(
-	   	'logged-in-menu' => __( 'Main Menu (Logged In)', 'child' ),
-	   	'logged-out-menu'  => __( 'Main Menu (Logged Out)', 'child' ),
-	) );
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('html5', array('search-form', 'gallery', 'caption'));
+    unregister_nav_menu('main-menu');
+    register_nav_menus(
+        array(
+            'logged-in-menu' => __( 'Main Menu (Logged In)', 'child' ),
+            'logged-out-menu'  => __( 'Main Menu (Logged Out)', 'child' ),
+        )
+    );
 });
 
 // 6. Admin Styles & Dark Mode
 add_action('admin_head', function () {
-	echo '<meta name="color-scheme" content="light dark" />';
+    echo '<meta name="color-scheme" content="light dark" />';
 });
 
 add_action('wp_enqueue_scripts', function() {
@@ -42,6 +45,6 @@ add_action('wp_enqueue_scripts', function() {
 });
 
 add_action('admin_enqueue_scripts', function () {
-	wp_enqueue_style('dashboard-css', get_stylesheet_directory_uri() . '/css/dashboard.css', array(), null);
-	wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap', false);
+    wp_enqueue_style('dashboard-css', get_stylesheet_directory_uri() . '/css/dashboard.css', array(), null);
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap', false);
 });
