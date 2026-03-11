46bdb14e1454eb6279f231917c4ee4cbd51f30d7
Chris Lowles <lowleschris@gmail.com>
2026-03-12 02:51:30 +1100

    Update functions.php

--- functions.php
+++ functions.php
@@ -25,11 +25,9 @@ add_action('after_setup_theme', function () {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_theme_support('html5', array('search-form', 'gallery', 'caption'));
-    unregister_nav_menu("main-menu");
     register_nav_menus(
         array(
-            'logged-in-menu'  => __( 'Main Menu (Logged In)', 'child' ),
-            'logged-out-menu' => __( 'Main Menu (Logged Out)', 'child' ),
+            'main-menu' => __( 'Main Menu', 'child' ),
         )
     );
 });
