59aef88d6d78a30ac17cea841b640328f247ea3d
Chris Lowles <lowleschris@gmail.com>
2026-03-20 01:47:35 +1100

    Update entry-meta.php

--- parts/entry-meta.php
+++ parts/entry-meta.php
@@ -1,9 +1,10 @@
 <!-- entry-meta.php -->
 <div class="d-flex align-items-center gap-2 small text-secondary mb-3">
     <span class="author">
-        <?php get_the_author_posts_link(); ?>
+        <?php the_author_posts_link(); ?>
     </span>
-    <div class="vr"></div> <span class="date">
+    <div class="vr"></div>
+    <span class="date">
         <a href="<?php the_permalink(); ?>" class="text-decoration-none text-secondary" title="<?php echo esc_attr( get_the_date() ); ?>">
             <?php the_time( get_option( 'date_format' ) ); ?>
         </a>
