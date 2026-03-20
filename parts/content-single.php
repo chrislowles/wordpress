54f23cc25d51d043d25aafc8934d56a5b76527db
Chris Lowles <lowleschris@gmail.com>
2026-03-20 17:24:56 +1100

    Update content-single.php

--- parts/content-single.php
+++ parts/content-single.php
@@ -1,25 +1,33 @@
 <!-- content-single.php -->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header>
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<?php get_template_part( 'parts/entry', 'meta' ); ?>
-	</header>
-	<div class="entry">
-		<div class="thumbnail">
-			<?php if ( has_post_thumbnail() ): ?>
-				<a class="thumbnail-link" href="<?php the_post_thumbnail_url( 'full' ); ?>" title="<?php $attachment_id = get_post_thumbnail_id( $post->ID ); the_title_attribute( array( 'post' => get_post( $attachment_id ) ) ); ?>">
-					<?php the_post_thumbnail( 'full'/*, array('itemprop' => 'image')*/ ); ?>
-				</a>
-			<?php endif; ?>
-		</div>
-		<div class="content">
-			<?php the_content(); ?>
-		</div>
-		<div class="links">
-			<?php wp_link_pages(); ?>
-		</div>
-	</div>
+    <header>
+        <h2>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+        </h2>
+        <?php get_template_part( 'parts/entry', 'meta' ); ?>
+    </header>
+    <div>
+        <div>
+            <?php if ( has_post_thumbnail() ): ?>
+                <a href="<?php the_post_thumbnail_url( 'full' ); ?>" title="<?php the_title_attribute(); ?>">
+                    <?php
+                        the_post_thumbnail(
+                            'full',
+                            [
+                                'class' => 'img-fluid',
+                                'title' => 'Thumbnail'
+                            ]
+                        );
+                    ?>
+                </a>
+            <?php endif; ?>
+        </div>
+        <div>
+            <?php the_content(); ?>
+        </div>
+        <div>
+            <?php wp_link_pages(); ?>
+        </div>
+    </div>
 </article>
 <!-- /content-single.php -->
