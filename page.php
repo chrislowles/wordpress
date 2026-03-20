d6cd4f9809876b70872e03454efbe7d6c1442a4a
Chris Lowles <lowleschris@gmail.com>
2026-03-21 03:02:07 +1100

    Create page.php

---
+++ page.php
@@ -0,0 +1,7 @@
+<!-- page.php -->
+<?php get_header(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <?php get_template_part( 'parts/content', 'page' ); ?>
+    <?php endwhile; endif; ?>
+<?php get_footer(); ?>
+<!-- /page.php -->
