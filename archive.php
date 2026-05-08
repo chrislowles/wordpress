1bdaff2bd4d41746b9b7ec542d002baec32be409
Chris Lowles <lowleschris@gmail.com>
2026-05-08 16:57:22 +1000

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,7 +1,13 @@
 <!-- archive.php -->
 <?php get_header(); ?>
 	<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
-		<?php get_template_part( 'parts/content', 'summary' ); ?>
+		<?php 
+			if ( 'show' === get_post_type() ) {
+				get_template_part( 'parts/content', 'show' );
+			} else {
+				get_template_part( 'parts/content', 'summary' );
+			}
+		?>
 	<?php endwhile; endif; ?>
 	<?php get_template_part( 'parts/nav', 'below' ); ?>
 <?php get_footer(); ?>
