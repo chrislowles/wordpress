a6c978d0643e6c2c3702c6cbe3a414a27ac414a7
Chris Lowles <lowleschris@gmail.com>
2026-05-13 02:48:57 +1000

    Update entry-meta.php

--- parts/entry-meta.php
+++ parts/entry-meta.php
@@ -1,5 +1,5 @@
 <!-- entry-meta.php -->
-<div class="d-flex align-items-center gap-2 small text-secondary mb-3">
+<div class="d-flex align-items-center flex-wrap gap-2 small text-secondary mb-3">
     <span class="author">
         <?php the_author_posts_link(); ?>
     </span>
@@ -9,11 +9,16 @@
             <?php the_time( get_option( 'date_format' ) ); ?>
         </a>
     </span>
-    <div class="vr"></div>
-    <?php $post_tags = get_the_tags(); if ( $post_tags ): ?>
-        <?php foreach ( $post_tags as $tag ) : ?>
-            <a href="<?php esc_url( get_tag_link( $tag->term_id ) ) ?>" class="badge text-bg-secondary text-decoration-none me-1"><?php esc_html( $tag->name ) ?></a>
-        <?php endforeach; ?>
+    <?php $post_tags = get_the_tags(); ?>
+    <?php if ( $post_tags ) : ?>
+        <div class="vr"></div>
+        <span class="tags d-flex flex-wrap gap-1">
+            <?php foreach ( $post_tags as $tag ) : ?>
+                <a href="<?php echo esc_url( get_tag_link( $tag->term_id ) ); ?>" class="badge text-bg-secondary text-decoration-none">
+                    <?php echo esc_html( $tag->name ); ?>
+                </a>
+            <?php endforeach; ?>
+        </span>
     <?php endif; ?>
 </div>
 <!-- /entry-meta.php -->
